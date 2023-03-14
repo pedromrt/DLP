e3f8215f1540320bfd36c612dcbe14f9af516a95
pedro.martz23@itca.edu.sv <pedro.martinez23@itca.edu.sv>
2023-03-14 16:41:08 -0600

    01

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -9,4 +9,5 @@ if  sueldoTraba>=550:
 else:
     sueldoNuevo=sueldoTraba
     print ("\n Su sueldo es de: $", sueldoTraba)
-    print ("\n Usted no aplica para el aumento.\n")
+    print ("\n Usted no aplica para el aumento.\n") 
+    
