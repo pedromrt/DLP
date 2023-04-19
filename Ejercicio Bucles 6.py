170b5b1c874e6c46d22ea98fca7838908ab7168c
Pedro Martinez <pedro.martinez23@itca.edu.sv>
2023-04-19 11:46:03 -0600

    Ejercicio Bucles 6 cambio 1

--- Ejercicio Bucles 6.py
+++ Ejercicio Bucles 6.py
@@ -1,8 +1,8 @@
-continuar = 1
+n = 1
 sumaSal = 0
-while continuar == 1:
-    n = float(input("\nEsbriba su salario: $"))
-    sumaSal = sumaSal + n
-    continuar = int( input("\n¿Desea continuar?\nEscriba 1 para Si y 0 para No: "))
+while n == 1:
+    salario = float(input("\nEsbriba su salario: $"))
+    sumaSal = sumaSal + salario
+    n = int( input("\n¿Desea continuar?\nEscriba 1 para Si y 0 para No: "))
 
 print ("\nEl salario total de la plantilla es de $", sumaSal,"\n")
