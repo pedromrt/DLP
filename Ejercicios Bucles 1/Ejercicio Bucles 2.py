83044f0723d473fb6465004e3493feba69f0a898
Pedro Martinez <pedro.martinez23@itca.edu.sv>
2023-04-24 07:04:59 -0600

    cambios 2 7

--- Ejercicios Bucles 1/Ejercicio Bucles 2.py
+++ Ejercicios Bucles 1/Ejercicio Bucles 2.py
@@ -3,17 +3,16 @@ positivos = 0
 negativos = 0
 nulos = 0
 
-for cont in range (1,6,1):
+for cont in range (1,101,1):
     numero = int(input("Ingrese un numero entero: "))
 
     if numero == 0:
         nulos = nulos + 1
-    elif numero < 0:
-        positivos = positivos + 1
     elif numero > 0:
+        positivos = positivos + 1
+    elif numero < 0:
         negativos = negativos + 1
 
 print ("\nUsted escribio",positivos, "positivos")
 print ("Usted escribio",negativos, "negativos")
 print ("Usted escribio",nulos, "nulos")
-
--- Ejercicios Bucles 1/Ejercicio Bucles 7.py
+++ Ejercicios Bucles 1/Ejercicio Bucles 7.py
@@ -1,7 +1,10 @@
 i = 2
 suma = 0
 while i <= 1800:
+    if i % 5 == 0:
+        i += 2
+    else:
+        i += 3
     suma = suma + i
-    i+=3
-
+    
 print("\nLa suma de la serie es igual a ", suma,"\n")
