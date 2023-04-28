89012e3a72dcda7ab426aac0193fcc46f612a19a
Pedro Martinez <pedro.martinez23@itca.edu.sv>
2023-04-27 19:51:56 -0600

    Cambios ejercicio 2

--- Ejercicios Listas, Tablas/Ejercicio Listas 2.py
+++ Ejercicios Listas, Tablas/Ejercicio Listas 2.py
@@ -6,7 +6,7 @@ while empleados == 1:
     lstNombres.append(input("\nIngrese el nombre del empleado: "))
     lstEdad.append(int(input("Ingrese la edad del empleado: ")))
     lstSalario.append(float(input("Ingrese el salario del empleado: $")))
-    empleados = int(input("\n¿Desea continuar? (Escriba 1 para si y 2 para no)\n1- Si\n2- No\n\n"))   
+    empleados = int(input("\n¿Desea añadir otro vector? (Escriba 1 para si y 2 para no)  "))   
 
 salarioMax = max(lstSalario)
 indice = lstSalario.index(salarioMax)
