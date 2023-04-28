c376eed781bf7d4c155c8fa2b1372d01e913ad04
Pedro Martinez <pedro.martinez23@itca.edu.sv>
2023-04-27 19:58:16 -0600

    ejercicio 1 listas cambio de for a while

--- Ejercicios Listas, Tablas/Ejercicio Listas 1.py
+++ Ejercicios Listas, Tablas/Ejercicio Listas 1.py
@@ -1,11 +1,14 @@
 lstNombres = []
 lstEdad = []
 lstSalario = []
+continuar = 1
 
-for i in range (0,5,1):
+while continuar <= 5:
     lstNombres.append(input("\nIngrese el nombre del empleado: "))
     lstEdad.append(int(input("Ingrese la edad del empleado: ")))
     lstSalario.append(float(input("Ingrese el salario del empleado: $")))
+    continuar += 1
+
 
 salarioMax = max(lstSalario)
 indice = lstSalario.index(salarioMax)
