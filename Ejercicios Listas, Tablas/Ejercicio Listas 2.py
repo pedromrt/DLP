2238903fd76ba1a262c17aaf0d06750716d0a0ee
Pedro Martinez <pedro.martinez23@itca.edu.sv>
2023-04-27 19:41:16 -0600

    Cambios ejercicio lista 2

--- Ejercicios Listas, Tablas/Ejercicio Listas 2.py
+++ Ejercicios Listas, Tablas/Ejercicio Listas 2.py
@@ -1,14 +1,14 @@
 lstNombres = []
 lstEdad = []
 lstSalario = []
-n = (int(input("Ingrese la cantidad de empleados: ")))
-
-for i in range(0,n,1):
+empleados = 1
+while empleados == 1:
     lstNombres.append(input("\nIngrese el nombre del empleado: "))
     lstEdad.append(int(input("Ingrese la edad del empleado: ")))
     lstSalario.append(float(input("Ingrese el salario del empleado: $")))
+    empleados = int(input("\n¿Desea continuar? (Escriba 1 para si y 2 para no)\n1- Si\n2- No\n\n"))   
 
 salarioMax = max(lstSalario)
 indice = lstSalario.index(salarioMax)
 
-print("\nEl empleado mejor pagado es " + lstNombres[indice] )
+print("\nEl empleado mejor pagado es " + lstNombres[indice])
