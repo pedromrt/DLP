11d2c28531b35f79d1065411d6343f677009a2ab
Pedro Martinez <pedro.martinez23@itca.edu.sv>
2023-06-09 15:59:05 -0600

    parcial 3

---
+++ Parcial 3/Ejercicio 1.py
@@ -0,0 +1,17 @@
+# Listas
+
+nombre = []
+
+sexo = []
+
+edad = []
+
+#MAIN MENU
+while True:
+
+    print ("----- HOSPITAL ROSALES -----")
+    print ("\n===== Añadir paciente ===== ")
+
+    nombre.append(input("\nIngrese el nombre del paciente: "))
+    print("\nSeleccione el genero del paciente: ")
+    print("\n1.Femenino")
