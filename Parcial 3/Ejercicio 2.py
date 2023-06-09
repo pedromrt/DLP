f48ff894afb845a22bbc91f93834aab76ca3f2cb
Pedro Martinez <pedro.martinez23@itca.edu.sv>
2023-06-09 17:51:14 -0600

    ejercicio 2 parcial 3

--- Parcial 3/Ejercicio 2.py
+++ Parcial 3/Ejercicio 2.py
@@ -16,6 +16,15 @@ def value_int_input(entrada):
     except ValueError:
         return entrada, False
 
+# Valida que la entrada sea de tipo float:
+def value_float_input(entrada):
+    try:
+        entrada = float(entrada)
+        return entrada, True
+    except ValueError:
+        return entrada, False
+
+
 # ENTRADAS
 
 # Verifica que la entrada no contenga solo espacios y que sea de tipo entero
@@ -35,3 +44,42 @@ def entrada_int_simple(mensaje):
         
         return entrada
 
+# Verifica que la entrada no contenga solo espacios y que sea de tipo float
+def entrada_float_simple(mensaje):
+    while True:
+        entrada = input(mensaje)
+        
+        entrada, check1 = no_spaces(entrada)
+        if check1 == False:
+            print("\nNo se permiten entradas vacias, intente de nuevo")
+            continue
+        
+        entrada, check2 = value_float_input(entrada)
+        if check2 == False:
+            print("\nTipo de entrada no valida, intente de nuevo")
+            continue
+        
+        return entrada
+
+
+# Listas 
+
+array_1 = []
+
+array_2 = []
+
+results_arrays = []
+
+# Length
+
+items = entrada_int_simple("\n\nIngrese la cantidad de datos a calcular: ")
+
+for i in range (items):
+    array_1.append(entrada_float_simple("\nIngrese el primer valor la resta " + str(i+1) + ": "))
+    array_2.append(entrada_float_simple("Ingrese el segundo valor la resta " + str(i+1) + ": "))
+    results_arrays.append(array_1[i] - array_2[i])
+
+for i in range (items):
+    print ("\nEl resultado de la resta " + str(i+1) + " es igual a " + str(results_arrays[i]))
+    
+print ()
